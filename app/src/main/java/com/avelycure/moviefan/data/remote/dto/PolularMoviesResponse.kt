3b32be1b11cb35b43b535bb095d2ae3265d4a1fd
avelycure <van.emelin2012@yandex.ru>
2021-10-19 08:06:52 +0300

    Added interface for network request

--- app/src/main/java/com/avelycure/moviefan/data/remote/dto/PolularMoviesResponse.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/PolularMoviesResponse.kt
@@ -3,7 +3,7 @@ package com.avelycure.moviefan.data.remote.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponsePolularMovies(
+data class PolularMoviesResponse(
     val page: Int,
     val results: List<MovieListResult>,
     val totalResults: Int,
