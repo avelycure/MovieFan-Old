2eaf405af6b4d8860f3b2c3e1a9d212b8ab96d90
avelycure <van.emelin2012@yandex.ru>
2021-10-19 08:02:34 +0300

    Add serializable annotations

--- app/src/main/java/com/avelycure/moviefan/data/remote/dto/MovieListResult.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/MovieListResult.kt
@@ -1,5 +1,8 @@
 package com.avelycure.moviefan.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class MovieListResult(
     val posterPath: String?,
     val adult: Boolean,
--- app/src/main/java/com/avelycure/moviefan/data/remote/dto/ResponsePolularMovies.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/ResponsePolularMovies.kt
@@ -1,5 +1,8 @@
 package com.avelycure.moviefan.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ResponsePolularMovies(
     val page: Int,
     val results: List<MovieListResult>,
