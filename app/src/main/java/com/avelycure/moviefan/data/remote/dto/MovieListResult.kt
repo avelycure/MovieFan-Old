8e061a8b6546156d2c67e71161bad91e3e719acb
avelycure <van.emelin2012@yandex.ru>
2021-10-19 08:00:51 +0300

    Added MovieListResult.kt dto Fixed typo

--- app/src/main/java/com/avelycure/moviefan/data/remote/dto/MovieListResult.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/MovieListResult.kt
@@ -1,5 +1,18 @@
 package com.avelycure.moviefan.data.remote.dto
 
 data class MovieListResult(
-    val posterPath: String?
+    val posterPath: String?,
+    val adult: Boolean,
+    val overview: String,
+    val releaseDate: String,
+    val genreIds: List<Int>,
+    val id: Int,
+    val originalTitle:String,
+    val originalLanguage: String,
+    val title: String,
+    val backdropPath: String?,
+    val popularity: Float,
+    val voteCount: Int,
+    val video: Boolean,
+    val voteAverage: Float
 )
