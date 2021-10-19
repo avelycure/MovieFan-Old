7a8d605cb4eb81e627ffcbdcbe5f35ecfc69e855
avelycure <van.emelin2012@yandex.ru>
2021-10-19 09:06:03 +0300

    Added dependency for activity ktx and MainActivityViewModel.kt to MainActivity.kt

--- app/src/main/java/com/avelycure/moviefan/presentation/MainActivity.kt
+++ app/src/main/java/com/avelycure/moviefan/presentation/MainActivity.kt
@@ -2,12 +2,17 @@ package com.avelycure.moviefan.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.avelycure.moviefan.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainActivityViewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/avelycure/moviefan/presentation/MainActivityViewModel.kt
+++ app/src/main/java/com/avelycure/moviefan/presentation/MainActivityViewModel.kt
@@ -1,7 +1,9 @@
 package com.avelycure.moviefan.presentation
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class MainActivityViewModel():ViewModel() {
 
 }
