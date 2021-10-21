650be368fde2033e1ce97b2976028d0dbd110a9b
avelycure <van.emelin2012@yandex.ru>
2021-10-21 22:22:53 +0300

    Added loading progress bar while loading images in first time

--- app/src/main/java/com/avelycure/moviefan/presentation/PopularMoviesFragment.kt
+++ app/src/main/java/com/avelycure/moviefan/presentation/PopularMoviesFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.core.widget.ContentLoadingProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.avelycure.moviefan.R
@@ -22,6 +26,7 @@ class PopularMoviesFragment : Fragment() {
     private lateinit var movieAdapter: PopularMovieAdapter
     private lateinit var rvPopularMovie: RecyclerView
     private val popularMoviesViewModel: PopularMoviesViewModel by viewModels()
+    private lateinit var loadingProgressBar: ProgressBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +35,26 @@ class PopularMoviesFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragement_popular_movies, container, false)
 
         rvPopularMovie = view.findViewById(R.id.rv_popular_movies)
+        loadingProgressBar = view.findViewById(R.id.fragment_pm_pb)
         rvPopularMovie.layoutManager = LinearLayoutManager(view.context)
         movieAdapter = PopularMovieAdapter()
+        movieAdapter.addLoadStateListener { loadState ->
+            if (loadState.refresh is LoadState.Loading){
+                loadingProgressBar.visibility = View.VISIBLE
+            }
+            else{
+                loadingProgressBar.visibility = View.GONE
+                val errorState = when {
+                    loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                    else -> null
+                }
+                errorState?.let {
+                    Toast.makeText(view.context, it.error.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
 
         val loadStateAdapter = movieAdapter.withLoadStateFooter(
             footer = MovieLoadStateAdapter { movieAdapter.retry() }
