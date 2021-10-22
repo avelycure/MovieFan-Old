60292ec7e5f76872e6c1997b3a12cd2b3b790980
avelycure <van.emelin2012@yandex.ru>
2021-10-22 21:14:56 +0300

    Removed class YTInitializedListener.kt, moved code to object in MovieInfoFragment.kt

--- app/src/main/java/com/avelycure/moviefan/presentation/movie_info/MovieInfoFragment.kt
+++ app/src/main/java/com/avelycure/moviefan/presentation/movie_info/MovieInfoFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.avelycure.moviefan.R
+import com.avelycure.moviefan.common.Constants
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import com.google.android.youtube.player.YouTubePlayerFragment
 import com.google.android.youtube.player.YouTubePlayerView
 
 class MovieInfoFragment : YouTubePlayerFragment() {
     private lateinit var youTubePlayerView: YouTubePlayerView
-    private lateinit var onInitializedListener: YTInitializedListener
+    private lateinit var onInitializedListener: YouTubePlayer.OnInitializedListener
 
     override fun onCreateView(
         inflater: LayoutInflater?,
@@ -20,6 +23,25 @@ class MovieInfoFragment : YouTubePlayerFragment() {
         val view = inflater!!.inflate(R.layout.fragment_movie_info, container, false)
 
         youTubePlayerView = view.findViewById(R.id.youtube_player_view)
+
+        onInitializedListener = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+
+            }
+
+            override fun onInitializationFailure(
+                provider: YouTubePlayer.Provider?,
+                result: YouTubeInitializationResult?
+            ) {
+
+            }
+        }
+
+        youTubePlayerView.initialize(Constants.YOUTUBE_API_KEY, onInitializedListener)
 
         return view
     }
