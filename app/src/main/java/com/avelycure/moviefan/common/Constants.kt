ba34bdf1c6bb0431bc557ab3c46b11f972d032e1
avelycure <van.emelin2012@yandex.ru>
2021-10-22 21:08:20 +0300

    Added YTInitializedListener

--- app/src/main/java/com/avelycure/moviefan/common/Constants.kt
+++ app/src/main/java/com/avelycure/moviefan/common/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     private const val BASE_URL = "https://api.themoviedb.org/3"
     const val POPULAR_MOVIES = "$BASE_URL/movie/popular?api_key=$API_KEY&page="
     const val IMAGE = "https://image.tmdb.org/t/p/w500"
+
+    const val YOUTUBE_API_KEY = "AIzaSyBPAAvMt6j3YtG1FsK7wmUso0L4wYhk-Zk"
 }
--- app/src/main/java/com/avelycure/moviefan/presentation/movie_info/MovieInfoFragment.kt
+++ app/src/main/java/com/avelycure/moviefan/presentation/movie_info/MovieInfoFragment.kt
@@ -1,4 +1,26 @@
 package com.avelycure.moviefan.presentation.movie_info
 
-class MovieInfoFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.avelycure.moviefan.R
+import com.google.android.youtube.player.YouTubePlayerFragment
+import com.google.android.youtube.player.YouTubePlayerView
+
+class MovieInfoFragment : YouTubePlayerFragment() {
+    private lateinit var youTubePlayerView: YouTubePlayerView
+    private lateinit var onInitializedListener: YTInitializedListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater?,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater!!.inflate(R.layout.fragment_movie_info, container, false)
+
+        youTubePlayerView = view.findViewById(R.id.youtube_player_view)
+
+        return view
+    }
 }
