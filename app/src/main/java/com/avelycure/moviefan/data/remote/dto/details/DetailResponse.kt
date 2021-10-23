9a6d0b059adb6ab393059bb342f5ed6185697a2b
avelycure <van.emelin2012@yandex.ru>
2021-10-23 13:48:56 +0300

    Added DetailResponse.kt

---
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/details/DetailResponse.kt
@@ -0,0 +1,25 @@
+package com.avelycure.moviefan.data.remote.dto.details
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DetailResponse(
+    val adult: Boolean,
+    val backdrop_path: String?,
+    val belongs_to_collection: DetailResponse?,
+    val budget: Int,
+    val homepage: String?,
+    val id: Int,
+    val imdb_id: String?,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Float,
+    val poster_path: String?,
+    val status: String,
+    val tagline: String?,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Float,
+    val vote_count: Int
+)
