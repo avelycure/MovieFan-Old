c599b88bab73c900a1427ab45e13e01c9e8a2931
avelycure <van.emelin2012@yandex.ru>
2021-10-23 14:17:43 +0300

    Fixed bug in MovieRepository.kt

--- app/src/main/java/com/avelycure/moviefan/data/remote/PostsService.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/PostsService.kt
@@ -7,5 +7,5 @@ import com.avelycure.moviefan.data.remote.dto.video.VideosResponse
 interface PostsService {
     suspend fun getPosts(nextPage: Int): PolularMoviesResponse
     suspend fun getVideos(id: Int): VideosResponse
-    suspend fun getMovieDetail(id: Int):DetailResponse
+    suspend fun getMovieDetail(id: Int): DetailResponse
 }
