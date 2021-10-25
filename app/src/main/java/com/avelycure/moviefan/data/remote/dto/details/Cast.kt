c08aa2de350fb4117e581789f116ab1dd2644055
avelycure <van.emelin2012@yandex.ru>
2021-10-25 20:38:25 +0300

    Added Cast.kt

---
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/details/Cast.kt
@@ -0,0 +1,16 @@
+package com.avelycure.moviefan.data.remote.dto.details
+
+data class Cast(
+    val adult: Boolean,
+    val gender: Int?,
+    val id: Int,
+    val known_for_department: String,
+    val name: String,
+    val original_name: String,
+    val popularity: Float,
+    val profile_path: String?,
+    val cast_id: Int,
+    val character: String,
+    val credit_id: String,
+    val order: Int
+)
