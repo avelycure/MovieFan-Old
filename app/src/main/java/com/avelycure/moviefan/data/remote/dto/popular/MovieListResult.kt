9af0e1e12d962e8564a7e3c854cadc896bf018cd
avelycure <van.emelin2012@yandex.ru>
2021-10-26 09:26:59 +0300

    Added mappers package to MovieListResult.kt

--- app/src/main/java/com/avelycure/moviefan/data/remote/dto/popular/MovieListResult.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/popular/MovieListResult.kt
@@ -21,16 +21,3 @@ data class MovieListResult(
     val vote_average: Float
 )
 
-fun MovieListResult.toPopularMovie(): PopularMovie {
-    return PopularMovie(
-        title = title,
-        originalTitle = original_title,
-        posterPath = poster_path,
-        genreIds = genre_ids,
-        popularity = popularity,
-        voteAverage = vote_average,
-        releaseDate = release_date,
-        movieId = id,
-        voteCount = vote_count
-    )
-}
---
+++ app/src/main/java/com/avelycure/moviefan/data/remote/dto/popular/mappers/MovieListToPopularMovie.kt
@@ -0,0 +1,19 @@
+package com.avelycure.moviefan.data.remote.dto.popular.mappers
+
+import com.avelycure.moviefan.data.remote.dto.popular.MovieListResult
+import com.avelycure.moviefan.domain.PopularMovie
+
+
+fun MovieListResult.toPopularMovie(): PopularMovie {
+    return PopularMovie(
+        title = title,
+        originalTitle = original_title,
+        posterPath = poster_path,
+        genreIds = genre_ids,
+        popularity = popularity,
+        voteAverage = vote_average,
+        releaseDate = release_date,
+        movieId = id,
+        voteCount = vote_count
+    )
+}
