04610981ac2fe3b47da4e4ddecce1ff7fd868dd0
avelycure <van.emelin2012@yandex.ru>
2021-10-30 17:28:42 +0300

    Filled DataState.kt

--- app/src/main/java/com/avelycure/moviefan/domain/DataState.kt
+++ app/src/main/java/com/avelycure/moviefan/domain/DataState.kt
@@ -1,6 +1,15 @@
 package com.avelycure.moviefan.domain
 
 sealed class DataState<T> {
+    data class Response<T>(
+        val uiComponent: UIComponent
+    ) : DataState<T>()
 
+    data class Data<T>(
+        val data: T? = null
+    ) : DataState<T>()
 
+    data class Loading<T>(
+        val progressBarState: ProgressBarState = ProgressBarState.Idle
+    ) : DataState<T>()
 }
