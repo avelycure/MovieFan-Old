e29023c08d8d4820fdb3c3505943cfe9e2698a31
avelycure <van.emelin2012@yandex.ru>
2021-10-30 17:28:17 +0300

    Added UIComponent.kt

---
+++ app/src/main/java/com/avelycure/moviefan/domain/UIComponent.kt
@@ -0,0 +1,13 @@
+package com.avelycure.moviefan.domain
+
+sealed class UIComponent {
+
+    data class Dialog(
+        val title: String,
+        val description: String,
+    ) : UIComponent()
+
+    data class None(
+        val message: String,
+    ) : UIComponent()
+}
