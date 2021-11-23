c7162a9aac6ba6c26e475eef227d74b73c8765d8
avelycure <van.emelin2012@yandex.ru>
2021-11-23 14:59:35 +0300

    Added methods to fetch popular movies from db

--- app/src/main/java/com/avelycure/moviefan/data/local/dao/CacheDao.kt
+++ app/src/main/java/com/avelycure/moviefan/data/local/dao/CacheDao.kt
@@ -1,9 +1,12 @@
 package com.avelycure.moviefan.data.local.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.avelycure.moviefan.data.local.entities.EntityMovie
+import com.avelycure.moviefan.data.local.entities.EntityPopularMovie
 
 @Dao
 interface CacheDao {
@@ -12,4 +15,13 @@ interface CacheDao {
 
     @Query("SELECT * FROM movies WHERE movieId = :movieId")
     fun getMovieInfo(movieId: Int):EntityMovie
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPopularMovies(movies: List<EntityPopularMovie>)
+
+    @Query("SELECT * FROM popular")
+    fun getPopularMovies(): PagingSource<Int, EntityPopularMovie>
+
+    @Query("DELETE FROM popular")
+    suspend fun clearPopularMovies()
 }
