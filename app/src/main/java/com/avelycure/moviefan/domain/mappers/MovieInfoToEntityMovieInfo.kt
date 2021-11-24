958b5bdc0c3d29cabaef4ff8cf8bef47adf795ed
avelycure <van.emelin2012@yandex.ru>
2021-11-24 16:42:48 +0300

    Added mapper MovieInfoToEntityMovieInfo.kt

---
+++ app/src/main/java/com/avelycure/moviefan/domain/mappers/MovieInfoToEntityMovieInfo.kt
@@ -0,0 +1,31 @@
+package com.avelycure.moviefan.domain.mappers
+
+import com.avelycure.moviefan.data.local.entities.EntityMovieInfo
+import com.avelycure.moviefan.domain.models.MovieInfo
+
+fun MovieInfo.toEntityMovieInfo(): EntityMovieInfo {
+    return EntityMovieInfo(
+        id = 0,
+        adult = adult,
+        budget = budget,
+        imdbId = imdbId,
+        originalLanguage = originalLanguage,
+        originalTitle = originalTitle,
+        overview = overview,
+        popularity = popularity,
+        genres = genres,
+        productionCompanies = productionCompanies,
+        productionCountries = productionCountries,
+        releaseDate = releaseDate,
+        spokenLanguages = spokenLanguages,
+        status = status,
+        revenue = revenue,
+        tagline = tagline,
+        voteAverage = voteAverage,
+        title = title,
+        voteCount = voteCount,
+        posterPath = posterPath,
+        cast = cast,
+        movieId = movieId
+    )
+}
