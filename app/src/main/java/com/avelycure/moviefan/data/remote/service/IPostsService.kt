efcb539c553df053fcce6362ef8ee2099e2b11d3
avelycure <van.emelin2012@yandex.ru>
2021-11-25 11:54:38 +0300

    Added method for fetching person images

--- app/src/main/java/com/avelycure/moviefan/data/remote/service/IPostsService.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/service/IPostsService.kt
@@ -2,6 +2,7 @@ package com.avelycure.moviefan.data.remote.service
 
 import com.avelycure.moviefan.data.remote.dto.details.DetailResponse
 import com.avelycure.moviefan.data.remote.dto.movie.MoviesResponse
+import com.avelycure.moviefan.data.remote.dto.person.PersonImages
 import com.avelycure.moviefan.data.remote.dto.video.VideosResponse
 
 interface IPostsService {
@@ -9,4 +10,5 @@ interface IPostsService {
     suspend fun getPopularMovies(nextPage: Int): MoviesResponse
     suspend fun getMovieDetail(id: Int): DetailResponse
     suspend fun getVideos(id: Int): VideosResponse
+    suspend fun getPersonImages(id: Int): PersonImages
 }
--- app/src/main/java/com/avelycure/moviefan/data/remote/service/PostsService.kt
+++ app/src/main/java/com/avelycure/moviefan/data/remote/service/PostsService.kt
@@ -3,6 +3,7 @@ package com.avelycure.moviefan.data.remote.service
 import com.avelycure.moviefan.common.Constants
 import com.avelycure.moviefan.data.remote.dto.details.DetailResponse
 import com.avelycure.moviefan.data.remote.dto.movie.MoviesResponse
+import com.avelycure.moviefan.data.remote.dto.person.PersonImages
 import com.avelycure.moviefan.data.remote.dto.video.VideosResponse
 import io.ktor.client.*
 import io.ktor.client.features.*
@@ -74,6 +75,22 @@ class PostsService(
             throw Exception("The server failed to fulfil an apparently valid request")
         } catch (e: IOException) {
             throw Exception("No internet connection")
+        }
+    }
+
+    override suspend fun getPersonImages(id: Int): PersonImages {
+        return try {
+            client.get {
+                url("${Constants.BASE_URL}/person/$id/images${Constants.API_KEY}")
+            }
+        } catch (e: RedirectResponseException) {
+            throw Exception("Further action needs to be taken in order to complete the request")
+        } catch (e: ClientRequestException) {
+            throw Exception("The request contains bad syntax or cannot be fulfilled")
+        } catch (e: ServerResponseException) {
+            throw Exception("The server failed to fulfil an apparently valid request")
+        } catch (e: IOException) {
+            throw IOException("No internet connection")
         }
     }
 }
