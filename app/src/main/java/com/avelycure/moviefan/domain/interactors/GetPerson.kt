03ba426e9e8b23067267eb99d15ccf8ade3b9df5
avelycure <van.emelin2012@yandex.ru>
2021-11-26 11:00:05 +0300

    Added interactor for fetching person information

---
+++ app/src/main/java/com/avelycure/moviefan/domain/interactors/GetPerson.kt
@@ -0,0 +1,11 @@
+package com.avelycure.moviefan.domain.interactors
+
+import com.avelycure.moviefan.data.repository.MovieRepository
+
+class GetPerson(
+    repository: MovieRepository
+) {
+    fun execute() {
+
+    }
+}
