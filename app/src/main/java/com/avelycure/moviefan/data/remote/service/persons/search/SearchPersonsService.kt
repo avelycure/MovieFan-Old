5c8c45e6838437803a40d97b11d576faf88faffa
avelycure <van.emelin2012@yandex.ru>
2021-11-30 23:52:07 +0300

    Extracted SearchPersonsService.kt

---
+++ app/src/main/java/com/avelycure/moviefan/data/remote/service/persons/search/SearchPersonsService.kt
@@ -0,0 +1,30 @@
+package com.avelycure.moviefan.data.remote.service.persons.search
+
+import com.avelycure.moviefan.common.RequestConstants
+import com.avelycure.moviefan.data.remote.dto.search_person.ResponseSearchPerson
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.utils.io.errors.*
+
+class SearchPersonsService(
+    private val client:HttpClient
+):ISearchPersonsService {
+    override suspend fun getPersonsByName(query: String, page: Int): ResponseSearchPerson {
+        return try {
+            client.get {
+                with(RequestConstants) {
+                    url("$BASE_URL/search/person?api_key=$API_KEY&query=${query}&page=${page}")
+                }
+            }
+        } catch (e: RedirectResponseException) {
+            throw Exception("Further action needs to be taken in order to complete the request")
+        } catch (e: ClientRequestException) {
+            throw Exception("The request contains bad syntax or cannot be fulfilled")
+        } catch (e: ServerResponseException) {
+            throw Exception("The server failed to fulfil an apparently valid request")
+        } catch (e: IOException) {
+            throw IOException("No internet connection")
+        }
+    }
+}
