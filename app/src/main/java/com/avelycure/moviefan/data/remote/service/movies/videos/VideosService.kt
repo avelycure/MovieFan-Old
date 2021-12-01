47078de5631ac152c5b660804b8d2beb4dea1ca9
avelycure <van.emelin2012@yandex.ru>
2021-12-01 10:20:50 +0300

    Added implementations for movies services

---
+++ app/src/main/java/com/avelycure/moviefan/data/remote/service/movies/videos/VideosService.kt
@@ -0,0 +1,31 @@
+package com.avelycure.moviefan.data.remote.service.movies.videos
+
+import com.avelycure.moviefan.common.RequestConstants
+import com.avelycure.moviefan.data.remote.dto.video.VideosResponse
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.request.*
+import io.ktor.utils.io.errors.*
+
+class VideosService(
+    private val client: HttpClient
+) : IVideosService {
+
+    override suspend fun getVideos(id: Int): VideosResponse {
+        return try {
+            client.get {
+                with(RequestConstants) {
+                    url("$BASE_URL/movie/$id/videos?api_key=$API_KEY")
+                }
+            }
+        } catch (e: RedirectResponseException) {
+            throw Exception("Further action needs to be taken in order to complete the request")
+        } catch (e: ClientRequestException) {
+            throw Exception("The request contains bad syntax or cannot be fulfilled")
+        } catch (e: ServerResponseException) {
+            throw Exception("The server failed to fulfil an apparently valid request")
+        } catch (e: IOException) {
+            throw Exception("No internet connection")
+        }
+    }
+}
